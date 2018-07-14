b136f32874d9215cf7dc8604fa1cebf86e5b8083
薛宇振 <fubar00098@gmail.com>
2018-07-14 21:13:12 +0800

    Final

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -9,17 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var funFactButton: UIButton!
+    @IBOutlet weak var funFactLabel: UILabel!
+    @IBOutlet weak var assignmentLabel: UILabel!
+    
+    let factProvider = FactProvider()
+    let colorProvider = BackgroundColorProvider()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        funFactLabel.text = factProvider.randomFact()
+        assignmentLabel.text = factProvider.randomText()
     }
 
 
+    
+    
+    @IBAction func showFact(_ sender: Any) {
+        print("You pressed me!")
+        
+        funFactLabel.text = factProvider.randomFact()
+        assignmentLabel.text = factProvider.randomText()
+        let randomColor = colorProvider.randomColor()
+        
+        // ViewController 不需知道隨機產生顏色如何計算，只需要將預呈現的資料放到view上，
+        // 所以另開Struct 在裡面做計算 接著在這邊使用。 跟要顯示Label文字資料儲存方式依樣。
+
+        self.view.backgroundColor = randomColor
+        funFactButton.tintColor = randomColor
+    }
 }
 
